f433dae91c9ebe5d5a014bbde41d5ad100ee2b58
yonshin <yonshin@student.42seoul.kr>
2023-10-19 10:24:15 +0900

    refactor: tile, player logic clean

--- level1/level1.gd
+++ level1/level1.gd
@@ -1,3 +1,4 @@
+class_name LevelMap
 extends TileMap
 
 const TILE_LAYER = 1
@@ -6,6 +7,9 @@ const FOG_TILE = Vector2i.ZERO
 const TILE_MAP = 0
 const TILE_MAP_FOG = 1
 var block_hp = [];
+
+@export var block_scene : PackedScene
+@export var player : NodePath
 
 func _enter_tree():
 	fill_fog()
@@ -18,7 +22,8 @@ func _ready():
 		block_hp.append([]);
 		for j in map_size.size.y:
 			block_hp[i].append(0);
-
+	var player_tile_pos = local_to_map(get_node(player).global_position)
+	reveal_fog(player_tile_pos)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -49,8 +54,7 @@ func get_surrounding_9cells(pos : Vector2i):
 	cells.append(pos + Vector2i(-1, -1))
 	return cells
 
-func reveal_fog(global_pos : Vector2):
-	var tile_pos = local_to_map(global_pos)
+func reveal_fog(tile_pos: Vector2i):
 	var fog = get_surrounding_9cells(tile_pos)
 	while fog.size() > 0:
 		var pos = fog.pop_back()
@@ -61,3 +65,29 @@ func reveal_fog(global_pos : Vector2):
 		if fog_tile != null:
 			if tile == null or tile.get_custom_data("is_wall") == false:
 				set_cells_terrain_connect(FOG_LAYER, [pos], TILE_MAP, -1, false)
+
+func damage_block(tile: Vector2i, power: int) -> bool:
+	var data = get_cell_tile_data(TILE_LAYER, tile)
+	if data == null:
+		return false
+	var block_hp = data.get_custom_data("hp")
+	if block_hp == 0:
+		return false
+	self.block_hp[tile.x][tile.y] += 1
+	if self.block_hp[tile.x][tile.y] >= block_hp:
+		set_cells_terrain_connect(TILE_LAYER, [tile], 0, -1, false)
+		reveal_fog(tile)
+		self.block_hp[tile.x][tile.y] = 0
+		var upCellPos = tile + Vector2i.UP;
+		var cell = get_cell_tile_data(TILE_LAYER, upCellPos)
+		if cell == null:
+			return true
+		if cell.get_custom_data("can_drop") == true:
+			var pos = map_to_local(upCellPos)
+			var block = block_scene.instantiate()
+			block.global_position = pos
+			get_node("/root/main").add_child(block)
+			
+			#await get_tree().create_timer(1).timeout
+			erase_cell(TILE_LAYER, upCellPos)
+	return true
--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,12 @@
 class_name Player
 extends CharacterBody2D
 
-const TILE_LAYER = 1;
+
 const SPEED = 100.0
 const JUMP_VELOCITY = -150.0
 var is_dig = false
 
-signal reveal_fog(reveal_pos : Vector2)
-
-@export var block_scene : PackedScene
+signal damage_block(pos: Vector2i, power: int)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -46,38 +44,11 @@ func _physics_process(delta):
 
 
 func wall_detect(body, dir_name):
-	if body.is_class("TileMap") and is_on_floor():
-		var tile_map : TileMap = body;
-		var checked_cell = tile_map.local_to_map(get_node(dir_name).global_position)
-		var data = tile_map.get_cell_tile_data(TILE_LAYER, checked_cell)
-		if data == null:
-			return
-		var block_hp = data.get_custom_data("hp")
-		if block_hp == 0:
-			return
-		tile_map.block_hp[checked_cell.x][checked_cell.y] += 1
-		is_dig = true
-		if tile_map.block_hp[checked_cell.x][checked_cell.y] >= block_hp:
-			tile_map.set_cells_terrain_connect(TILE_LAYER, [checked_cell], 0, -1, false)
-			emit_signal("reveal_fog", get_node(dir_name).global_position)
-			tile_map.block_hp[checked_cell.x][checked_cell.y] = 0
-			var upCellPos = checked_cell + Vector2i.UP;
-			var cell = tile_map.get_cell_tile_data(TILE_LAYER, upCellPos)
-			if cell == null:
-				return;
-			if cell.get_custom_data("can_drop") == true:
-				#tile_map.set_cell(0, upCellPos, 0, Vector2i(3, 6))
-				#await get_tree().create_timer(.5).timeout
-				
-				#tile_map.erase_cell(0, upCellPos)
-
-				var pos = tile_map.map_to_local(upCellPos)
-				var block = block_scene.instantiate()
-				block.global_position = pos
-				get_node("/root/main").add_child(block)
-				
-				await get_tree().create_timer(1).timeout
-				tile_map.erase_cell(TILE_LAYER, upCellPos)
+	if is_on_floor() and body is LevelMap:
+		var map : LevelMap = body;
+		var checked_cell = map.local_to_map(get_node(dir_name).global_position)
+		is_dig = map.damage_block(checked_cell, 1)
+		#map.damage_block(checked_cell, 1)
 				
 func _gameover():
 	get_tree().reload_current_scene()
